ab541884ed4dd688153f73f766dd56ae77afcbae
Deddy Syefria (deedee) <dsyefria@gmail.com>
2022-11-29 15:06:56 +0700

    importdatalama: add check

--- geonode/geokincia/management/commands/importdatalama.py
+++ geonode/geokincia/management/commands/importdatalama.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
             "Import data lama ke layer yang sudah ada field foto/video: Foto, Foto1, Foto2, FotoX,... Video, Video1,Video2,..."
         )
 
-    def _process_csv(self, source, target, att_path):
+    def _process_csv(self, source, target, att_path, att_check=False):
         rows = []
         with open(source, 'r') as f:
             reader = csv.reader(f, dialect='excel')
@@ -42,9 +42,8 @@ class Command(BaseCommand):
                         row[field] = row[field][row[field].index(':')+2:]
                     except:
                         pass
-                    print(row[field])
                     if not os.path.exists(os.path.join(att_path, row[field])):
-                        print(f'file {row[field]} does not exist')
+                        print(f'{row[1]:} file {row[field]} -> {os.path.exists(os.path.join(att_path, row[field]))} does not exist')
                     new_att.append(os.path.join(att_path, row[field]))
             for field in fields:
                 row[field:field+1] = []
@@ -62,26 +61,30 @@ class Command(BaseCommand):
             for row in rows:
                 writer.writerow(row)
 
-        db_utils.load_from_csv('datastore', os.path.join(os.path.dirname(source), '___out2.csv'), target, False, None, True)
+        if not att_check:
+            db_utils.load_from_csv('datastore', os.path.join(os.path.dirname(source), '___out2.csv'), target, False, None, True)
 
-    def _process_shp(self, source, target, att_path):
+    def _process_shp(self, source, target, att_path, att_check=False):
         if os.path.exists(source + '.csv'):
             os.remove(source + '.csv')
         r = subprocess.run(f'ogr2ogr -f CSV -overwrite {os.path.basename(source)}.csv  {os.path.basename(source)} -lco GEOMETRY=AS_WKT -t_srs "EPSG:4326" ', capture_output=True, shell=True, cwd=os.path.dirname(source))
         if r.returncode != 0:
             print(f'Gagal process file {source}')
             return
-        self._process_csv(os.path.join(os.path.dirname(source), os.path.basename(source) + '.csv'), target, att_path)
+        self._process_csv(os.path.join(os.path.dirname(source), os.path.basename(source) + '.csv'), target, att_path, att_check=att_check)
 
     def add_arguments(self, parser):
         parser.add_argument("-t", "--target", dest="target", help="Name layer target.")
 
         parser.add_argument("-s", "--source", dest="source", help="File sumber shp/csv.")
 
+        parser.add_argument("-c", "--check", dest="check", help="Check attachment")
+
     
     def handle(self, *args, **options):
         target = options.get("target")
         source = options.get("source")
+        check = True if options.get("check").lower() == 'y' else False
         path = '.'
 
         if not target or not source:
@@ -91,8 +94,9 @@ class Command(BaseCommand):
         if not os.path.exists(source):
             print('source not found')
             return
-        
+
+                
         if source.lower().endswith('.shp'):
-            self._process_shp(source, target, path)
+            self._process_shp(source, target, path, att_check=check)
         elif source.lower().endswith('.csv'):
-            self._process_csv(source, target, path)
+            self._process_csv(source, target, path, att_check=check)
